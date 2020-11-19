3430408120258207e003a82dc5b3735df3e1c785
AthinaVek <athina.vekraki@gmail.com>
2020-11-19 14:27:07 +0200

    Loop and menu

--- autoencoder.py
+++ autoencoder.py
@@ -66,81 +66,93 @@ if __name__ == "__main__":
 	train_ground = np.reshape(train_ground, (len(train_ground), 28, 28, 1))
 	valid_ground = np.reshape(valid_ground, (len(valid_ground), 28, 28, 1))
 
-	layers = int(input("GIVE NUMBER OF LAYERS: \n"))
-	filters_size = int(input("GIVE FILTER SIZE: \n"))
-	filters_num = int(input("GIVE NUMBER OF FILTERS IN FIRST LAYER: \n"))
-	epochs_num = int(input("GIVE NUMBER OF EPOCHS: \n"))
-	batch_sz = int(input("GIVE BATCH SIZE: \n"))
-	
-	# layers = 5
-	# filters_size = 3
-	# filters_num = 8
-	# epochs_num = 10
-	# batch_sz = 100
+	while (1):
+		layers = int(input("GIVE NUMBER OF LAYERS: \n"))
+		filters_size = int(input("GIVE FILTER SIZE: \n"))
+		filters_num = int(input("GIVE NUMBER OF FILTERS IN FIRST LAYER: \n"))
+		epochs_num = int(input("GIVE NUMBER OF EPOCHS: \n"))
+		batch_sz = int(input("GIVE BATCH SIZE: \n"))
+		
+		# layers = 5
+		# filters_size = 3
+		# filters_num = 8
+		# epochs_num = 10
+		# batch_sz = 100
 
-	count = 0
-	input = keras.layers.Input(shape=(28, 28, 1), name='input')
-	x = input
+		count = 0
+		networkInput = keras.layers.Input(shape=(28, 28, 1), name='input')
+		x = networkInput
 
-	for l in range(layers):							# ENCODER
-		conv_name = 'conv_' + str(l+1)
+		for l in range(layers):							# ENCODER
+			conv_name = 'conv_' + str(l+1)
 
-		x = keras.layers.Conv2D(filters_num, kernel_size=(filters_size,filters_size), padding = 'same', activation='relu', name=conv_name)(x)
-		x = keras.layers.BatchNormalization()(x)
-		if (count < 3):
-			x = keras.layers.MaxPooling2D(pool_size=(2,2), padding='same')(x)
-			count = count+1
-		x = keras.layers.Dropout(0.5)(x)
-		filters_num = filters_num*2
-	filters_num = filters_num/2
-
-
-	for l in range(layers-1):							# DECODER
-		conv_name = 'conv_' + str(l+layers+1)
-
-		x = keras.layers.Conv2D(filters_num, kernel_size=(filters_size,filters_size), padding = 'same', activation='relu', name=conv_name)(x)
-		x = keras.layers.BatchNormalization()(x)
-		if (count-1 > 0):
-			x = keras.layers.UpSampling2D(size=(2,2))(x)
-			count = count-1
-
+			x = keras.layers.Conv2D(filters_num, kernel_size=(filters_size,filters_size), padding = 'same', activation='relu', name=conv_name)(x)
+			x = keras.layers.BatchNormalization()(x)
+			if (count < 3):
+				x = keras.layers.MaxPooling2D(pool_size=(2,2), padding='same')(x)
+				count = count+1
+			x = keras.layers.Dropout(0.5)(x)
+			filters_num = filters_num*2
 		filters_num = filters_num/2
-	conv_name = 'conv_' + str(l+layers+2)
-	x = keras.layers.Conv2D(filters_num, kernel_size=(filters_size,filters_size), activation='relu', name=conv_name)(x)
-	x = keras.layers.BatchNormalization()(x)
-	x = keras.layers.UpSampling2D(size=(2,2))(x)
-	output = keras.layers.Conv2D(filters=1, kernel_size=(filters_size,filters_size), padding='same', activation='sigmoid', name='output')(x)
 
 
-	model = Model(inputs=input, outputs=output, name='AUTOENCODER')
-	model.compile(optimizer=keras.optimizers.RMSprop(), loss='mean_squared_error', metrics=['accuracy'])
-	model.summary()
+		for l in range(layers-1):							# DECODER
+			conv_name = 'conv_' + str(l+layers+1)
 
-	history = model.fit(x_test, valid_ground, batch_size=batch_sz, epochs=epochs_num, shuffle=True, verbose=1, validation_data=(x_test, valid_ground))
-	out_images = model.predict(x_test)
+			x = keras.layers.Conv2D(filters_num, kernel_size=(filters_size,filters_size), padding = 'same', activation='relu', name=conv_name)(x)
+			x = keras.layers.BatchNormalization()(x)
+			if (count-1 > 0):
+				x = keras.layers.UpSampling2D(size=(2,2))(x)
+				count = count-1
 
-	n = 10  # How many digits we will display
-	plt.figure(figsize=(20, 4))
-	for i in range(n):
-		# Display original
-		ax = plt.subplot(2, n, i + 1)
-		plt.imshow(x_test[i].reshape(28, 28))
-		plt.gray()
-		ax.get_xaxis().set_visible(False)
-		ax.get_yaxis().set_visible(False)
+			filters_num = filters_num/2
+		conv_name = 'conv_' + str(l+layers+2)
+		x = keras.layers.Conv2D(filters_num, kernel_size=(filters_size,filters_size), activation='relu', name=conv_name)(x)
+		x = keras.layers.BatchNormalization()(x)
+		x = keras.layers.UpSampling2D(size=(2,2))(x)
+		output = keras.layers.Conv2D(filters=1, kernel_size=(filters_size,filters_size), padding='same', activation='sigmoid', name='output')(x)
 
-		# Display reconstruction
-		ax = plt.subplot(2, n, i + 1 + n)
-		plt.imshow(out_images[i].reshape(28, 28))
-		plt.gray()
-		ax.get_xaxis().set_visible(False)
-		ax.get_yaxis().set_visible(False)
-	plt.show()
 
-	plt.plot(history.history['loss'])
-	plt.title('model loss')
-	plt.ylabel('loss')
-	plt.xlabel('epoch')
-	plt.legend(['train', 'test'], loc='upper left')
-	plt.show()
+		model = Model(inputs=networkInput, outputs=output, name='AUTOENCODER')
+		model.compile(optimizer=keras.optimizers.RMSprop(), loss='mean_squared_error', metrics=['accuracy'])
+		model.summary()
 
+		history = model.fit(x_test, valid_ground, batch_size=batch_sz, epochs=epochs_num, shuffle=True, verbose=1, validation_data=(x_test, valid_ground))
+		out_images = model.predict(x_test)
+
+
+		val = int(input("TO REPEAT THE EXPERIMENT PRESS 1.\nTO SHOW THE PLOTS PRESS 2.\nTO SAVE THE MODEL PRESS 3.\n"))
+		if(val == 1):
+			continue
+		elif(val == 2):
+			# n = 10  # How many digits we will display
+			# plt.figure(figsize=(20, 4))
+			# for i in range(n):
+			# 	# Display original
+			# 	ax = plt.subplot(2, n, i + 1)
+			# 	plt.imshow(x_test[i].reshape(28, 28))
+			# 	plt.gray()
+			# 	ax.get_xaxis().set_visible(False)
+			# 	ax.get_yaxis().set_visible(False)
+
+			# 	# Display reconstruction
+			# 	ax = plt.subplot(2, n, i + 1 + n)
+			# 	plt.imshow(out_images[i].reshape(28, 28))
+			# 	plt.gray()
+			# 	ax.get_xaxis().set_visible(False)
+			# 	ax.get_yaxis().set_visible(False)
+			# plt.show()
+
+			plt.plot(history.history['loss'])
+			plt.title('model loss')
+			plt.ylabel('loss')
+			plt.xlabel('epoch')
+			plt.legend(['train', 'test'], loc='upper left')
+			plt.show()
+			val = int(input("TO REPEAT THE EXPERIMENT PRESS 1.\nTO SAVE THE MODEL PRESS 3.\n"))
+			if(val == 1):
+				continue
+			elif(val == 3):
+				break
+		elif(val == 3):
+			break
